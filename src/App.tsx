9b6817ced83d81153341a28141981d7c0e26cd26
DevAnupShourya <contact.yourwebdev@gmail.com>
2023-07-18 12:00:01 +0530

    initail : project configured successfully

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// ? Local Files
+import './style/tailwindcss.css';
+// ? packages
+import {  } from "react-router-dom";
+
+
+export default  function App() {
+  return (
+    <h1 className='text-red-700'>Hello World</h1>
+  )
+}
---
+++ src/assets/constants.ts
@@ -0,0 +1,116 @@
+export const hero_section = {
+	title: "hello i'm anup shourya",
+	tagline: "unleashing passibilities through passionate programming craftsmanship"
+};
+export const navbar_links = [
+	{
+		link_name: "home",
+		link_address: "/"
+	},
+	{
+		link_name: "about",
+		link_address: "/about"
+	},
+	{
+		link_name: "projects",
+		link_address: "/projects"
+	},
+	{
+		link_name: "blogs",
+		link_address: "/blogs"
+	},
+	{
+		link_name: "contact",
+		link_address: "/contact"
+	},
+];
+export const social_links = [
+	{
+		social_media_name: "github",
+		social_media_link: "https://github.com/AnupShouryaDev",
+	},
+	{
+		social_media_name: "linkedin",
+		social_media_link: "",
+	},
+	{
+		social_media_name: "twitter",
+		social_media_link: "",
+	},
+	{
+		social_media_name: "email",
+		social_media_link: "mailto:contact.yourwebdev@gmail.com",
+	},
+];
+export const about_section = {
+	whoIam: {
+		myName: "Anup shourya",
+		myStatus: "freelancer at upwork",
+		mySelf: "lorem lore"
+	},
+	howIWork: {
+		step1: {
+			name: "Designing",
+			details: "lamba chouda kaam hai jo karnan padta hai"
+		},
+		step2: {
+			name: "programming",
+			details: "lamba chouda kaam hai jo karnan padta hai"
+		},
+		step3: {
+			name: "deployment",
+			details: "lamba chouda kaam hai jo karnan padta hai"
+		}
+	},
+	whatIknow: [
+		{
+			techName: "reactjs",
+			techIcon: "<arreactjs/>"
+		},
+		{
+			techName: "reactjs",
+			techIcon: "<arreactjs/>"
+		},
+		{
+			techName: "reactjs",
+			techIcon: "<arreactjs/>"
+		},
+		{
+			techName: "reactjs",
+			techIcon: "<arreactjs/>"
+		},
+	]
+};
+export const projects_section = [
+	{
+		projectName: "google adds",
+		projectSummary: "lorem labma cahuda hai",
+		projectGithubLink: "https//www.github.com/yele",
+		projectLivedemoLink: "https//www.yele.com/",
+		projectImgLink: "./src/assests/yele.jpg",
+	},
+	{
+		projectName: "google adds",
+		projectSummary: "lorem labma cahuda hai",
+		projectGithubLink: "https//www.github.com/yele",
+		projectLivedemoLink: "https//www.yele.com/",
+		projectImgLink: "./src/assests/yele.jpg",
+	},
+	{
+		projectName: "google adds",
+		projectSummary: "lorem labma cahuda hai",
+		projectGithubLink: "https//www.github.com/yele",
+		projectLivedemoLink: "https//www.yele.com/",
+		projectImgLink: "./src/assests/yele.jpg",
+	},
+	{
+		projectName: "google adds",
+		projectSummary: "lorem labma cahuda hai",
+		projectGithubLink: "https//www.github.com/yele",
+		projectLivedemoLink: "https//www.yele.com/",
+		projectImgLink: "./src/assests/yele.jpg",
+	},
+];
+export const footer_section = {
+	footerMsg: "ye project  hum banai hai samajh me aya jya"
+};
